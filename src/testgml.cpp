82ec17848e6c22ef17fda5b678d54a6461fbfd83
Mirko Ferrati <mirko.ferrati@gmail.com>
2015-12-15 19:52:48 +0100

    cleaned code

--- src/testgml.cpp
+++ src/testgml.cpp
@@ -2,39 +2,58 @@
 #include <fstream>
 #include <lemon/smart_graph.h>
 #include <lemon/graph_to_eps.h>
+#include <lemon/lgf_reader.h>
 #include <gmlreader/gmlreader.hpp>
 #include <graph_msgs/GeometryGraph.h>
 #include <ros/ros.h>
+#include <ros/package.h>
 // #include <rviz_visual_tools/rviz_visual_tools.h>
 
 using namespace std;
-#define SCALE 0.05 //TODO ros param
+// #define SCALE 0.05 //TODO ros param
 
 int main(int argc, char **argv) {
+    ros::init(argc,argv,"testgml");
+    ros::NodeHandle n;
+    double scale;
+    ros::param::get("graph_scale",scale);
     lemon::SmartDigraph g;
     lemon::SmartDigraph::NodeMap<int> a(g),b(g);
     lemon::SmartDigraph::ArcMap<int> c(g);
-    ifstream f;
+    
     if (argc<2)
     {
         cout<< "usage: test_gml filename.gml"<<endl;
         return 1;
     }
-    string filename=argv[1];
-    cout<<filename<<endl;
-    
-    f.open(filename.c_str(),ios::in);
-    
-    ros::init(argc,argv,"testgml");
-    ros::NodeHandle n;
-    gmlreader reader;
-    reader.read(&f,g);
-    reader.getCoordx(a);
-    reader.getCoordy(b);
-    reader.getArcLength(c);
-    
-    f.close();
-
+    std::string graphName = argv[1];
+    std::string path = ros::package::getPath("map_database");
+    std::cout<<path<<"/"<<graphName<<std::endl;
+    if (graphName.rfind(".lgf")==graphName.length()-4)
+    {
+        typedef lemon::dim2::Point<int> Point;
+        lemon::SmartDigraph::NodeMap<Point> coords(g);
+        lemon::DigraphReader<lemon::SmartDigraph>(g, path+"/"+graphName).
+        nodeMap("coordinates",coords).
+        run();
+        for (lemon::SmartDigraph::NodeIt n(g);n!=lemon::INVALID;++n)
+        {
+            a[n]=coords[n].x;
+            b[n]=coords[n].y;
+        }
+    }
+    if (graphName.rfind(".gml")==graphName.length()-4)
+    {
+        ifstream f;
+        f.open((path+"/"+graphName).c_str(),ios::in);
+        if (f.fail()) abort();
+        gmlreader reader;
+        reader.read(&f,g);
+        reader.getCoordx(a);
+        reader.getCoordy(b);
+        reader.getArcLength(c);
+        f.close();
+    }
     graph_msgs::GeometryGraph g_msg;
     
     std::map<int,int> nodes_id_to_index;
@@ -48,8 +67,8 @@ int main(int argc, char **argv) {
         id[n]=g.id(n);
         geometry_msgs::Point p;
         graph_msgs::Edges empty_edge;
-        p.x=SCALE*double(a[n]);
-        p.y=SCALE*double(b[n]);
+        p.x=scale*double(a[n]);
+        p.y=scale*double(b[n]);
         p.z=id[n];
         g_msg.nodes.push_back(p);
         g_msg.edges.push_back(empty_edge);
@@ -70,28 +89,28 @@ int main(int argc, char **argv) {
     ros::AsyncSpinner as(1);as.start();
     while(ros::ok())
     {
-        sleep(1);
-    pub.publish(g_msg);
-}
+        sleep(10);
+        pub.publish(g_msg);
+    }
 //     for (auto& p:g_msg.nodes)
 //         p.z=0;
 //     rviz_visual_tools::RvizVisualTools tools("map","graph_markers");
 //     tools.publishGraph(g_msg,rviz_visual_tools::GREY,0.2);
 //     sleep(1);
-    lemon::graphToEps<lemon::SmartDigraph> ( g,"image.eps" ).
-    coords ( coords ).
-    nodeTexts ( id ).
-    nodeTextSize ( 4 ).
-    nodeScale ( 0.008 ).
-    arcWidthScale ( 0.0008 ).
-    drawArrows ( true ).
-    arrowWidth ( 3 ).
-    arrowLength ( 5 ).
-    enableParallel ( true ).
-    distantColorNodeTexts().
-    run();
-    
-    std::cout << "A digraph is read from "<<filename << std::endl;
+//     lemon::graphToEps<lemon::SmartDigraph> ( g,"image.eps" ).
+//     coords ( coords ).
+//     nodeTexts ( id ).
+//     nodeTextSize ( 4 ).
+//     nodeScale ( 0.008 ).
+//     arcWidthScale ( 0.0008 ).
+//     drawArrows ( true ).
+//     arrowWidth ( 3 ).
+//     arrowLength ( 5 ).
+//     enableParallel ( true ).
+//     distantColorNodeTexts().
+//     run();
+//     
+    std::cout << "A digraph is read from "<<graphName << std::endl;
     std::cout << "Number of nodes: " << lemon::countNodes ( g ) << std::endl;
     std::cout << "Number of arcs: " << lemon::countArcs ( g ) << std::endl;
     return 0;
