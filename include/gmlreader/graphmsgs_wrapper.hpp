d5f5fbfb74e60cfe2ad49adeb69ecc60be461988
Alessandro <ale.settimi@gmail.com>
2015-12-17 18:30:57 +0100

    made it compile, added scale in read

--- include/gmlreader/graphmsgs_wrapper.hpp
+++ include/gmlreader/graphmsgs_wrapper.hpp
@@ -6,16 +6,14 @@
 class graphmsgs_gml
 {
     gmlreader reader;
-    gml_utils::graph g;
-    graph_msgs::GeometryGraph gr;
     std::map<int,int> id_to_index;
 public:
     graphmsgs_gml()
     {
         
-    };
+    }
     
-    void convert(const gml_utils::graph& g, const graph_msgs::GeometryGraph& gr,double scale=1)
+    void convert(const gml_utils::graph& g, graph_msgs::GeometryGraph& gr,double scale=1)
     {
         int i=0;
         for (auto node : g.nodes)
@@ -29,27 +27,28 @@ public:
         gr.edges.resize(g.nodes.size());
         for (auto arc: g.arcs )
         {
-            gr.edges[id_to_index[arc.first]].push_back(id_to_index[arc.second]);
+            gr.edges[id_to_index[arc.first]].node_ids.push_back(id_to_index[arc.second]);
         }
     }
     
-    void read(std::ifstream& f,graph_msgs::GeometryGraph& gr)
+    void read(std::ifstream& f,graph_msgs::GeometryGraph& gr,double scale=1)
     {
         reader.read(f);
-        g=reader.getGraph();
+        gml_utils::graph g=reader.getGraph();
         //convert g to lemon graph
+	int i=0;
         for (auto node : g.nodes)
         {
             geometry_msgs::Point p;
-            p.x=node.x;
-            p.y=node.y;
+            p.x=node.x*scale;
+            p.y=node.y*scale;
             gr.nodes.push_back(p);
             id_to_index[node.index]=i++;
         }
         gr.edges.resize(g.nodes.size());
         for (auto arc: g.arcs )
         {
-            gr.edges[id_to_index[arc.first]].push_back(id_to_index[arc.second]);
+            gr.edges[id_to_index[arc.first]].node_ids.push_back(id_to_index[arc.second]);
         }
     }
     
